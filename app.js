8571c236b6ee7838c364dcdb4fc57f8139485f19
Serhii Bordeniuk <brdnk.srh@gmail.com>
2023-10-11 13:35:51 +0300

    error handler added

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const path = require("path");
+
 const express = require("express");
 
 const bodyParser = require("body-parser");
@@ -8,6 +10,7 @@ const feedRoutes = require("./routes/feed");
 const app = express();
 
 app.use(bodyParser.json());
+app.use("/images", express.static(path.join(__dirname, "images")));
 
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
@@ -17,6 +20,13 @@ app.use((req, res, next) => {
 });
 
 app.use("/feed", feedRoutes);
+
+app.use((error, req, res, next) => {
+    console.log(error);
+    const status = error.statusCode || 500;
+    const message = error.message;
+    res.status(status).json({ messahe: message });
+});
 
 mongoose
     .connect(
