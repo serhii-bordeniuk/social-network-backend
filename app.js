6cdcc3b39987474690e9c790fcf153eb468c6265
Serhii Bordeniuk <brdnk.srh@gmail.com>
2023-11-13 15:27:39 +0200

    login and created post added

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const { graphqlHTTP } = require("express-graphql");
 
 const graphqlSchema = require("./graphql/schema");
 const graphqlResolver = require("./graphql/resolvers");
+const auth = require("./middleware/auth");
 
 const app = express();
 
@@ -41,8 +42,13 @@ app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    if (req.method === "OPTIONS") {
+        return res.sendStatus(200);
+    }
     next();
 });
+
+app.use(auth);
 
 app.use(
     "/graphql",
@@ -52,7 +58,7 @@ app.use(
         graphiql: true,
         customFormatErrorFn: (err) => ({
             message: err.message || "An error occurred.",
-            code: err.originalError.code || 500,
+            code: err.originalError.code || 500, 
             data: err.originalError.data,
         }),
     })
--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,7 +1,9 @@
 const bcrypt = require("bcryptjs");
 const validator = require("validator");
+const jwt = require("jsonwebtoken");
 
 const User = require("../models/user");
+const Post = require("../models/post");
 
 module.exports = {
     createUser: async function ({ userInput }, req) {
@@ -43,6 +45,76 @@ module.exports = {
         return {
             ...createdUser._doc,
             _id: createdUser._id.toString(),
+        };
+    },
+    login: async function ({ email, password }) {
+        const user = await User.findOne({ email: email });
+        if (!user) {
+            const error = new Error("User not found");
+            error.code = 401;
+            throw error;
+        }
+        const isEqual = await bcrypt.compare(password, user.password);
+        if (!isEqual) {
+            const error = new Error("Password is incorrect");
+            error.code = 401;
+            throw error;
+        }
+        const token = jwt.sign(
+            {
+                userId: user._id.toString(),
+                email: user.email,
+            },
+            "somesupersupersecret",
+            { expiresIn: "1h" }
+        );
+        return { token: token, userId: user._id.toString() };
+    },
+    createPost: async function ({ postInput }, req) {
+        if (!req.isAuth) {
+            const error = new Error("Not authenticated");
+            error.code = 401;
+            throw error;
+        }
+        const errors = [];
+        if (
+            validator.isEmpty(postInput.title) ||
+            !validator.isLength(postInput.title, { min: 5 })
+        ) {
+            errors.push({ message: "Title is invalid" });
+        }
+        if (
+            validator.isEmpty(postInput.content) ||
+            !validator.isLength(postInput.content, { min: 5 })
+        ) {
+            errors.push({ message: "Content is invalid" });
+        }
+        if (errors.length > 0) {
+            const error = new Error("Invalid input");
+            error.data = errors;
+            error.code = 422;
+            throw error;
+        }
+        const user = await User.findById(req.userId);
+        if (!user) {
+            const error = new Error("Invalid user");
+            error.data = errors;
+            error.code = 401;
+            throw error;
+        }
+        const post = new Post({
+            title: postInput.title,
+            content: postInput.content,
+            imageUrl: postInput.imageUrl,
+            creator: user,
+        });
+        const createdPost = await post.save();
+        user.posts.push(createdPost); 
+        return {
+            ...createdPost._doc,
+            _id: createdPost._id.toString(),
+            createdAt: createdPost.createdAt.toISOString(),
+            updatedAt: createdPost.updatedAt.toISOString(),
         };
     },
 };
