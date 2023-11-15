ca1c09d0f1b53b0ba2f1cedf446e294c9cf8a2c6
Serhii Bordeniuk <brdnk.srh@gmail.com>
2023-11-15 13:26:30 +0200

    deleting post added

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const path = require("path");
-const fs = require("fs");
+
 
 const express = require("express");
 
@@ -11,6 +11,7 @@ const { graphqlHTTP } = require("express-graphql");
 const graphqlSchema = require("./graphql/schema");
 const graphqlResolver = require("./graphql/resolvers");
 const auth = require("./middleware/auth");
+const { clearImage } = require('./util/file')
 
 const app = express();
 
@@ -95,7 +96,3 @@ mongoose
     })
     .catch((err) => console.log(err));
 
-const clearImage = (filePath) => {
-    filePath = path.join(__dirname, ".", filePath);
-    fs.unlink(filePath, (err) => console.log(err));
-};
